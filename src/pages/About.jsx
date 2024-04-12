2f55486da039230bd75fffbe4055788c63f9d2bb
Dhruv Bonde <devnev39@gmail.com>
2024-04-12 08:55:00 +0530

    chore: added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {MDBTypography} from 'mdb-react-ui-kit';
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='d-flex justify-content-center align-items-center' style={{height: "80vh"}}>
+      <MDBTypography className='mark'>
+        Feedback counting app !
+      </MDBTypography>
+    </div>
   )
 }
